4683637aad26eabc094a06c1031a6fa44efe849e
ShrutiNim <152527973+ShrutiNim@users.noreply.github.com>
2024-02-21 12:27:20 +0530

    Add files via upload

---
+++ App.py/app.py
@@ -0,0 +1,59 @@
+# app.py
+
+from flask import Flask, render_template, Response
+import cv2
+import numpy as np
+from keras.preprocessing import image
+from keras.models import load_model
+
+app = Flask(__name__)
+
+# Load the emotion detection model
+model_path = "best_model.h5"
+model = load_model(model_path)
+
+face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+        ret, test_img = cap.read()
+        if not ret:
+            continue
+        gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2RGB)
+
+        faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+
+        for (x, y, w, h) in faces_detected:
+            cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
+            roi_gray = gray_img[y:y + w, x:x + h]
+            roi_gray = cv2.resize(roi_gray, (224, 224))
+            img_pixels = image.img_to_array(roi_gray)
+            img_pixels = np.expand_dims(img_pixels, axis=0)
+            img_pixels /= 255
+
+            predictions = model.predict(img_pixels)
+            max_index = np.argmax(predictions[0])
+
+            emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+            predicted_emotion = emotions[max_index]
+
+            cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+        resized_img = cv2.resize(test_img, (1000, 700))
+        _, jpeg = cv2.imencode('.jpg', resized_img)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
